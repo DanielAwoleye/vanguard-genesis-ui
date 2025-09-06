c0ac538d92910b15a1621ed06e64743a93d61707
DanielAwoleye <ayodeji.awoleye@gmail.com>
2025-09-06 06:49:55 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neon: {
+          pink: "#ff00ff",
+          cyan: "#00ffff",
+          blue: "#00bfff"
+        }
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ],
+}
 
